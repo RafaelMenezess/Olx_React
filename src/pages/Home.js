7cace0f83abaa8af5aaf49440dd8ee5dd188c1e3
RafaelMenezess <rafa.rcm@hotmail.com>
2020-08-05 16:51:22 -0300

    Estruturando as Rotas

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+
+            <Link to="/sobre">Ir para página SOBRE</Link>
+        </div>
+    );
+};
+
+export default Home;
